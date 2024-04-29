a7bebfab888582af1113511f1b42c4389f564926
PhcNguyen <phcnguyenz@proton.me>
2024-04-29 16:17:00 +0700

    1.0.2

--- .github/run.py
+++ .github/run.py
@@ -1,3 +1,6 @@
+import sys
+import os.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from subprocess import run
 
 
---
+++ static/home/js/home.js
@@ -0,0 +1,9 @@
+function toggleSocialLinks() {
+    var links = document.getElementById('social-links');
+    if (links.style.display === 'block') {
+      links.style.display = 'none';
+    } else {
+      links.style.display = 'block';
+    }
+}
+  
