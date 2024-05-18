5d6d06dc6525e36749a5d8f7d9bfd2d4c345a1aa
Pngz <phcnguyenz@proton.me>
2024-05-18 17:25:43 +0700

    1.0.5

--- modules/system/utils.py
+++ modules/system/utils.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+from pathlib import Path
+
 from .colors import Colors as Col
 
 
@@ -8,3 +12,19 @@ FRAMES = [
     f"\r {Col.White}[{Col.Yellow}{' ' * i}<--{' ' * (5 - i)}{Col.White}]"
     for i in range(5, -1, -1)
 ]
+
+
+def remove_pycache(directory):
+    """
+    from pathlib import Path
+    remove_pycache(Path.cwd())
+    """
+    for root, dirs, files in os.walk(directory):
+        for dir_name in dirs:
+            if dir_name == "__pycache__":
+                pycache_dir = os.path.join(root, dir_name)
+                shutil.rmtree(pycache_dir)
+        for file_name in files:
+            if file_name.endswith(".pyc") or file_name.endswith(".pyo"):
+                pyc_file = os.path.join(root, file_name)
+                os.remove(pyc_file)
