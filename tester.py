573e45a8d3b4a8d815ba57c9cd1557749a69ea91
PhcNguyen <phcnguyenz@proton.me>
2024-05-20 11:30:24 +0700

    1.0.5

--- tester.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-settings.configure('freya/settings.py')
-
-from django.utils import timezone
-import datetime
-print(timezone.now())
-
-print(timezone.make_aware(datetime.datetime.now(),timezone.get_default_timezone()))
