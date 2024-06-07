c8c3290124ae239546e8d3768d5c63b91edefccd
PHUC NGUYEN <138844322+PhcNguyen@users.noreply.github.com>
2024-06-07 20:24:49 +0700

    1.0.6

--- manage.py
+++ manage.py
@@ -4,37 +4,35 @@ import sys
 
 from modules import ui
 from modules.core import Terminal, System
+from modules.settings import ERROR_MESSAGE
 
 try:
     from django.core.management import execute_from_command_line
     from django.core.management.commands.runserver import Command
     Terminal.Console('DJANGO', 'Green', 'Django setup successfully.')
 except ImportError:
-    Terminal.Console("DJANGO", "Red",
-        "Đã có lỗi không mong muốn xảy ra! "
-        + "Hãy liên hệ với Developers để khắc phục sự cố."
-    )
+    Terminal.Console("DJANGO", "Red", ERROR_MESSAGE)
     Terminal.Exit()
 
 
 def main():
     Terminal.Clear()
     ui.home()
-    Terminal.Console('SERVER', 'Green', 'Đang khởi tạo server.')
+    Terminal.Console('SERVER', 'Green', 'Bắt đầu khởi động server.')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "freya.settings")
 
     try:
         Command.default_addr = System.local_ip()
         Command.default_port = "8080"
 
-        Terminal.Console('SERVER', 'Green', f'Server IP: {Command.default_addr}')
-        Terminal.Console('SERVER', 'Green', f'Server Port: {Command.default_port}')
+        Terminal.Console('SERVER', 'Green', f'IP: {Command.default_addr}')
+        Terminal.Console('SERVER', 'Green', f'Port: {Command.default_port}')
 
         execute_from_command_line(
             [sys.argv[0], "runserver"]
         )
-    except Exception as e:
-        Terminal.Console("SERVER", "Red", f"Không thể khởi động server: {e}")
+    except:
+        Terminal.Console("SERVER", "Red", ERROR_MESSAGE)
         Terminal.Exit()
 
 
--- modules/settings.py
+++ modules/settings.py
@@ -3,6 +3,9 @@ import os.path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DEBUG = False
 
 UI = os.path.join(BASE_DIR, 'modules', 'ui', 'ui.json')
+
+
+ERROR_MESSAGE = "Đã có lỗi không mong muốn xảy ra! Hãy liên hệ với Developers để khắc phục sự cố."
